fd48c9b93f78b4894102d6333d3a7eb940094152
Shayne Clausson <sclausson@hotmail.com>
2015-07-30 16:13:51 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ansible-code-commit
---
+++ spec/54.175.115.79/cc_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe file('/root/.ssh/cc_ssh_user.pem') do 
+  it { should exist }
+  it { should be_mode 400}
+end
+
+describe file('/root/.ssh/config') do 
+  it { should exist }
+  it { should contain 'User APKAJJXESLIAPY7RHHOA'}
+end
+
+describe command('ssh-keygen -H -F git-codecommit.us-east-1.amazonaws.com') do
+  its(:exit_status) { should eq 0 }
+end
+
+describe package('git') do
+  it { should be_installed }
+end
+
+describe command('aws --version') do
+  its(:stdout) { should contain('aws-cli/1.7.41') }
+end
+
+describe file('/srv/git/repos/hello-world/hello.txt') do
+  it { should exist }
+  it { should contain 'Hello World!' }
+end
